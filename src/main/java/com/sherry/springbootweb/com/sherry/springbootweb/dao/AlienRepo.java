946864197ec553fa7de38bfed20b1a15749b3731
Sehajpreet Singh <iamsherrysingh@gmail.com>
2019-05-25 23:19:16 -0400

    Code cleanup.

--- src/main/java/com/sherry/springbootweb/com/sherry/springbootweb/dao/AlienRepo.java
+++ src/main/java/com/sherry/springbootweb/com/sherry/springbootweb/dao/AlienRepo.java
@@ -3,13 +3,8 @@ package com.sherry.springbootweb.com.sherry.springbootweb.dao;
 import com.sherry.springbootweb.model.Alien;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-//import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "aliens",path="aliens")
 public interface AlienRepo extends JpaRepository<Alien, Integer> {
-//    public List<Alien> findByalang(String alang);
-//    List<Alien> findByAidGreaterThan(int aid);
-//    List<Alien> findByAidLessThanEqual(int aid);
+
 }
