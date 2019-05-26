dcc89a294dbc4bea4230269cb4947283bb8e342f
Sehajpreet Singh <iamsherrysingh@gmail.com>
2019-05-25 23:11:32 -0400

    Removed a '}' after a sql query.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,4 +2,4 @@ insert into alien values(1,'java','sherry');
 insert into alien values(2,'perl','kammu');
 insert into alien values(3,'java','aman');
 insert into alien values(4,'java script', 'abhi');
-insert into alien values(5,'ruby','shota');}
+insert into alien values(5,'ruby','shota');
